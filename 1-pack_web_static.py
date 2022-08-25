947d1325a0fb454a9e08c6602abe498779ef63dd
Astambar <leuchartjason@gmail.com>
2022-08-25 10:50:52 +0000

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,13 +6,13 @@ mon clone statique AirBnB
 """
 
 from fabric.api import local
-from datetime import datetime
-date = datetime.now()
-complete = date.strftime("%Y%m%d%H%M%S")
+import time
+complete = time.strftime("%Y%m%d%H%M%S")
+
 def do_pack():
     try:
         local('mkdir -p versions')
-        local("tar -cvzf versions/web_static_{}.tgz web_static".format(complete))
-        return "versions/web_static_{}.tgz".format(complete)
+        local(f"tar -cvzf versions/web_static_{complete}.tgz web_static")
+        return f"versions/web_static_{complete}.tgz"
     except(Exception):
         return None
