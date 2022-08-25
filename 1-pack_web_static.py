81a9d89e2416a0e9c938e3c3297537e6c79c2057
Astambar <leuchartjason@gmail.com>
2022-08-25 11:01:29 +0000

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,10 +9,10 @@ from fabric.api import local
 import time
 
 def do_pack():
-    complete = time.strftime("%Y%m%d%H%M%S")
+    date = time.strftime("%Y%m%d%H%M%S")
     try:
         local('mkdir -p versions')
-        local('tar -czvf versions/web_static_{}.tgz web_static'.format(complete))
-        return 'versions/web_static_{}.tgz'.format(complete)
+        local('tar -czvf versions/web_static_{}.tgz web_static'.format(date))
+        return 'versions/web_static_{}.tgz'.format(date)
     except Exception:
         return None
