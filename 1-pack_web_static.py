97bd0eada350c657fa7c73f18a51c2897bbd7447
Astambar <leuchartjason@gmail.com>
2022-08-25 10:52:59 +0000

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,9 +7,9 @@ mon clone statique AirBnB
 
 from fabric.api import local
 import time
-complete = time.strftime("%Y%m%d%H%M%S")
 
 def do_pack():
+    complete = time.strftime("%Y%m%d%H%M%S")
     try:
         local('mkdir -p versions')
         local(f"tar -cvzf versions/web_static_{complete}.tgz web_static")
