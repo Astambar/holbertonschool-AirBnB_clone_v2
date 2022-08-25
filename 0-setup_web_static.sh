5a2a2b9118ee6a983e52586b5eda97c2c1631c1c
Astambar <leuchartjason@gmail.com>
2022-08-25 15:34:20 +0000

    probleme vm

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -26,20 +26,10 @@ do
 		touch "$element"
 		fi
 done
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>
-" >> /data/web_static/releases/test/index.html
 if [ -L "data/web_static/current" ]; then
-
+echo "suppression du liens symbolic"
 unlink data/web_static/current
 fi
+echo création du liens
 ln -s data/web_static/releases/test/ data/web_static/current
-chown -hR ubuntu:ubuntu /data/
-
-sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
-service nginx restart
+sudo service nginx restart
