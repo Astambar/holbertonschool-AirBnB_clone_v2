5ce80a092bfb9522dede89907b4b3669b776a9a7
Astambar <leuchartjason@gmail.com>
2022-08-25 11:09:52 +0000

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
 
 """
-Créez une archive .tgz avec tout le contenu statoc de
-mon clone statique AirBnB
+Create an .tgz archive with all the statoc content of
+my AirBnB static clone
 """
 
 from fabric.api import local
 import time
 
+
 def do_pack():
     """
-    The do_pack function creates a tarball containing the web_static folder.
-    It returns the name of the tarball it created, or None if there was an error.
-
-    :return: The path to the tarball file
-    :doc-author: Trelent
+    Enpack all the static content of AirBnB clone
     """
-
-    complete = time.strftime("%Y%m%d%H%M%S")
+    date = time.strftime("%Y%m%d%H%M%S")
     try:
         local('mkdir -p versions')
-        local(f'tar -cvzf versions/web_static_{complete}.tgz web_static')
-        return f'versions/web_static_{complete}.tgz'
-    except(Exception):
+        local('tar -czvf versions/web_static_{}.tgz web_static'.format(date))
+        return 'versions/web_static_{}.tgz'.format(date)
+    except Exception:
         return None
