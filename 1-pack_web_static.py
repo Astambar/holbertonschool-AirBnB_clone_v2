58a6fe7945aec632bfa21bea521f7a758e4b2e59
Astambar <leuchartjason@gmail.com>
2022-08-25 10:54:30 +0000

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,7 +12,7 @@ def do_pack():
     complete = time.strftime("%Y%m%d%H%M%S")
     try:
         local('mkdir -p versions')
-        local(f"tar -cvzf versions/web_static_{complete}.tgz web_static")
-        return f"versions/web_static_{complete}.tgz"
+        local(f'tar -cvzf versions/web_static_{complete}.tgz web_static')
+        return f'versions/web_static_{complete}.tgz'
     except(Exception):
         return None
